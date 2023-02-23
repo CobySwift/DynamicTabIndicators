8327bfc40602062fef24fa5c598c9749de524ec3
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-02-24 01:40:49 +0900

    Initial Commit

---
+++ DynamicTabIndicators/DynamicTabIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorsApp.swift
+//  DynamicTabIndicators
+//
+//  Created by COBY_PRO on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
